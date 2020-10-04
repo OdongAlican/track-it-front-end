320a8530a1a01f41bbca96603bcfbce0069530f8
OdongAlican <sandieo.2020@gmail.com>
2020-10-04 23:39:57 +0300

    add signup form

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from 'react-redux'
+import { signUpAction } from '../../actions'
 
+const Signup = ( props ) => {
 
-const Signup = () => {
+    let { history } = props
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [age, setAge] = useState(0)
+    const dispatch = useDispatch()
+
+    const submitData = (event) => {
+        event.preventDefault()
+        dispatch(signUpAction({username, password, age}, history))
+    }
+
     return(
         <div>
+        <form onSubmit = { submitData }>
+            <label htmlFor="username">User Name</label>
+            <input value = { username } onChange={ e => setUsername(e.target.value) }></input>
+            <label htmlFor="password"> Password</label>
+            <input value = { password } onChange= { e => setPassword(e.target.value )}></input>
+            <label htmlFor="age"> Age </label>
+            <input value = { age } onChange = { e => setAge(e.target.value)}></input>
+            <button type="submit">Submit</button>
+        </form> 
         </div>
     )
 }
