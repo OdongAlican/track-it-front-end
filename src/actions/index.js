2a326b7e7b4d39f355808b5aadead2ecd2242e08
OdongAlican <sandieo.2020@gmail.com>
2020-10-04 16:15:02 +0300

    add the actions file

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+export const AUTHENTICATED = 'authenticated_user';
+export const UNAUTHENTICATED = 'unauthenticated_user';
+export const AUTHENTICATION_ERROR = 'authentication_error';
+
+const URL = 'http://localhost:3000';
+
+
+export function signInAction({ username, password }, history){
+    return async (dispatch) => {
+        const response = await axios.post(`${URL}/login`, { username, password })
+        if (response.data.token) {
+            dispatch({ type: AUTHENTICATED });
+            localStorage.setItem('user', response.data.token);
+            history.push('/activities')
+        } else {
+            dispatch({
+                type: AUTHENTICATION_ERROR,
+                payload: 'Invalid username or Password'
+            })
+            history.push('/signin')
+        }
+    }
+}
