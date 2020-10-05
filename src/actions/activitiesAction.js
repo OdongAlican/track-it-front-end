4dbf3e1be154b8014f4e00b70d5ebbb443f874e7
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 22:30:42 +0300

    add deleteActivity and createActivity actions

--- src/actions/activitiesAction.js
+++ src/actions/activitiesAction.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 // export const FETCH_ACTIVITIES_REQUEST = 'FETCH_ACTIVITIES_REQUEST'
 export const FETCH_ACTIVITIES_SUCCESS = 'FETCH_ACTIVITIES_SUCCESS'
 export const FETCH_ACTIVITIES_FAILURE = 'FETCH_ACTIVITIES_FAILURE'
+export const DELETE_ACTIVITY = 'DELETE_ACTIVITY'
+export const CREATE_ACTIVITY = 'CREATE_ACTIVITY'
 
 const URL = 'http://localhost:3000/activities'
 
@@ -16,8 +18,35 @@ export const fetchActivitiesFailure = error => ({
     payload: error
 })
 
+export const deleteActivity = (id) => {
+    axios.delete(`${URL}/${id}`, { 
+        headers: {"Authorization" : `Bearer ${localStorage.user}`} 
+    } )
+    return {
+        type: DELETE_ACTIVITY,
+        payload: id
+    }
+}
+
+export const createActivity = ({ title }, history) => {
+    const response = axios.post(URL, { title },{ 
+        headers: {"Authorization" : `Bearer ${localStorage.user}`} 
+    })
+
+    if(response){
+        history.push('/activities')
+    }
+    return {
+        type: CREATE_ACTIVITY,
+        payload: { title }
+    }
+}
+
 export const fetchActivities = () => dispatch => {
-    axios.get(URL, { headers: {"Authorization" : `Bearer ${localStorage.user}`} }).then( response => {
+    axios.get(
+          URL, 
+        { headers: {"Authorization" : `Bearer ${localStorage.user}`} }
+        ).then( response => {
         const activities = response.data;
         dispatch(fetchActivitiesSuccess(activities))
     }).catch(error => {
