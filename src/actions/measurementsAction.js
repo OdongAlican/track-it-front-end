f65a20a517b6b3060821c57ba5d59e2644945017
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 22:25:39 +0300

    add measurement actions

---
+++ src/actions/measurementsAction.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+export const FETCH_MEASUREMENTS_SUCCESS = 'FETCH_MEASUREMENTS_SUCCESS'
+export const FETCH_MEASUREMENTS_FAILURE = 'FETCH_MEASUREMENTS_FAILURE'
+
+const URL = 'http://localhost:3000/activities'
+
+
+export const fetchMeasurementsSuccess = measurements => ({
+    type: FETCH_MEASUREMENTS_SUCCESS,
+    payload: measurements
+})
+
+export const fetchMeasurementsFailure = error => ({
+    type: FETCH_MEASUREMENTS_FAILURE,
+    payload: error
+})
+
+export const fetchMeasurements = (id) => dispatch => {
+    axios.get(`${URL}/${id}/measurements`, 
+        { headers: {"Authorization" : `Bearer ${localStorage.user}`} }
+        ).then( response => {
+        const measurements = response.data;
+        dispatch(fetchMeasurementsSuccess(measurements))
+    }).catch(error => {
+        const errMsg = error
+        dispatch(fetchMeasurementsFailure(errMsg))
+    })
+
+}
