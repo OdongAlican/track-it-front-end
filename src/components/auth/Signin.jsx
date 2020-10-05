955ae7b109781ab70c5f7c29fee6ffa8f025bea5
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 11:50:55 +0300

    add style to the sign in form

--- src/components/auth/Signin.jsx
+++ src/components/auth/Signin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { signInAction } from '../../actions'
 import { useDispatch, useSelector } from 'react-redux'
+import Signup from './Signup'
 
 const Signin = (props) =>{
     let { history } = props
@@ -16,14 +17,18 @@ const Signin = (props) =>{
     const errorMessage = useSelector(state => state.authReducer.error)
 
     return (
-        <div>
-            <div>
-            <form onSubmit={ saveData }>
-                <input value = { username } onChange={(e) => setUsername(e.target.value)}></input>
-                <input value = { password } onChange={(e) => setPassword(e.target.value)} type="password"></input>
-                <p>{ errorMessage ? errorMessage : '' }</p>
-                <button type="submit">Submit</button> 
-            </form>
+        <div className="d-flex">
+            <div className="bg-secondary col-md-6 border-right p-3">
+                <h3>Log In</h3>
+                <form onSubmit={ saveData }>
+                    <input placeholder="Enter User Name" className="form-control mb-2 col-md-6" value = { username } onChange={(e) => setUsername(e.target.value)}></input>
+                    <input placeholder="Enter Password" className="form-control col-md-6" value = { password } onChange={(e) => setPassword(e.target.value)} type="password"></input>
+                    <p className="text-danger">{ errorMessage ? errorMessage : '' }</p>
+                    <button type="submit" className="btn btn-primary">Submit</button> 
+                </form>
+            </div>
+            <div className="bg-secondary col-md-6 p-3">
+                <Signup history = { history }/>
             </div>
         </div>
     )
