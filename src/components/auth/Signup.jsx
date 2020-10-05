825bf0f7d43b20282db553b91b34bb333bce389b
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 11:51:21 +0300

    add style to the sign up form

--- src/components/auth/Signup.jsx
+++ src/components/auth/Signup.jsx
@@ -7,7 +7,7 @@ const Signup = ( props ) => {
     let { history } = props
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
-    const [age, setAge] = useState(0)
+    const [age, setAge] = useState('')
     const dispatch = useDispatch()
 
     const submitData = (event) => {
@@ -17,14 +17,24 @@ const Signup = ( props ) => {
 
     return(
         <div>
+            <h3>Sign Up</h3>
         <form onSubmit = { submitData }>
-            <label htmlFor="username">User Name</label>
-            <input value = { username } onChange={ e => setUsername(e.target.value) }></input>
-            <label htmlFor="password"> Password</label>
-            <input value = { password } onChange= { e => setPassword(e.target.value )}></input>
-            <label htmlFor="age"> Age </label>
-            <input value = { age } onChange = { e => setAge(e.target.value)}></input>
-            <button type="submit">Submit</button>
+            <input 
+            placeholder="Enter User Name" 
+            className="form-control col-md-6 mb-2"
+            value = { username } 
+            onChange={ e => setUsername(e.target.value) }></input>
+            <input 
+            placeholder="Enter Password"  
+            className="form-control col-md-6 mb-2"
+            value = { password } 
+            onChange= { e => setPassword(e.target.value )}></input>
+            <input  
+            placeholder="Enter Age" 
+            className="form-control col-md-6"
+            value = { age } 
+            onChange = { e => setAge(e.target.value)}></input>
+            <button type="submit" className="btn btn-primary mt-2">Submit</button>
         </form> 
         </div>
     )
