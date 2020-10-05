35fb233e8d96e3ba0e3bef6054a7c5301c4396bf
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 22:32:47 +0300

    update activity component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { deleteActivity } from '../actions/activitiesAction'
+import { Link } from "react-router-dom";
 
 const Activity = ({ activity }) => {
+
+    const dispatch = useDispatch()
+
+    const deleteAct = (id) => {
+        dispatch(deleteActivity(id))
+    }
 
     return (
         <div className="card col-md-3 mt-2" >
@@ -8,8 +17,16 @@ const Activity = ({ activity }) => {
                 <h5 className="card-title">{ activity.title }</h5>
                 <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a className="card-link">Edit</a>
-                <a className="card-link">Delete</a>
+                <button className="btn btn-secondary mr-2">Edit</button>
+                <button className="btn btn-info mr-2">
+                    <Link className="text-white" to={{
+                        pathname: `/activity/${activity.id}/measurements`,
+                        state: activity.id 
+                    }}>
+                        View Details
+                    </Link>
+                    </button>
+                <button className="btn btn-danger" onClick = {() => deleteAct(activity.id) }>Delete</button>
             </div>
         </div>
     )
