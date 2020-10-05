a53cb13273e214f844a11be6aeb066dfbe8c231a
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 22:27:09 +0300

    add Measurement component

---
+++ src/components/Measurement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Measurement = ({ measurement }) => {
+    return(
+        <div>
+            <p> {measurement.duration} </p>
+        </div>
+    )
+}
+
+export default Measurement
