e6cd678c348a569feb9a065d7534b1875f210d59
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 22:34:03 +0300

    update create activity and measurement routes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -4,6 +4,8 @@ import App from './App'
 import Signin from './auth/Signin'
 import Activities from '../container/Activities'
 import Navbar from './Navbar'
+import CreateActivity from './CreateActivity'
+import Measurements from '../container/Measurements'
 
 const Router = () => {
     return (
@@ -14,6 +16,8 @@ const Router = () => {
                     <Route path="/" component={ App } exact/>
                     <Route path="/signin" component= { Signin }/>
                     <Route path="/activities" component= { Activities }/>
+                    <Route path="/create-activity" component={CreateActivity}/>
+                    <Route path="/activity/:id/measurements" component={ Measurements }/>
                 </Switch>
             </BrowserRouter>
         </div>
