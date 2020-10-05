10a28ca9577971239d19a95db364a3b2eb2850ad
OdongAlican <sandieo.2020@gmail.com>
2020-10-05 11:47:18 +0300

    add a Navbar to the application

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return(
+        <div className="col-md-12 d-flex pt-4 bg-info text-white justify-content-center">
+            <h3>Track it</h3>
+        </div>
+    )
+}
+
+export default Navbar
