cfa193633a9cc3244ee1abe3dd28c5c1ae96c7f2
OdongAlican <sandieo.2020@gmail.com>
2020-10-06 13:07:43 +0300

    update Measurements container

--- src/container/Measurements.jsx
+++ src/container/Measurements.jsx
@@ -2,18 +2,29 @@ import React, { useEffect } from 'react'
 import Measurement from '../components/Measurement'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchMeasurements } from '../actions/measurementsAction'
+import { Link } from 'react-router-dom'
 
 const Measurements = (props) => {
-    const actID = props.location.state
+    const activityId = props.location.state
     const dispatch = useDispatch()
 
     useEffect(()=> {
-        dispatch(fetchMeasurements(actID))
+        dispatch(fetchMeasurements(activityId))
     }, [])
     const measurements = useSelector(state => state.measurementsReducer.measurements)
     
     return (
         <div>
+            <div>
+                <button className= "btn btn-primary mt-2 ml-2 "> 
+                    <Link className="text-white" to={{
+                        pathname: `/activity/${activityId}/create`,
+                        state : activityId
+                    }}>
+                        Add Measurement
+                    </Link>
+                 </button>
+            </div>
             {
                 measurements.map((measurement, key) => (
                     <Measurement key = { key } measurement = { measurement } />
