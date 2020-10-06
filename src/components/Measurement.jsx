a47c6bed5096b87804e58a3ea146484f129081f3
OdongAlican <sandieo.2020@gmail.com>
2020-10-06 13:05:54 +0300

    add styles to Measurement component

--- src/components/Measurement.jsx
+++ src/components/Measurement.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Measurement = ({ measurement }) => {
     return(
         <div>
-            <p> {measurement.duration} </p>
+            <div className="card col-md-4 mt-2 ml-2">
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"> { measurement.duration } </li>
+                    <li className="list-group-item">{ measurement.created_at }</li>
+                </ul>
+            </div>
         </div>
     )
 }
