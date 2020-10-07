b7a072fed2110e7a014995f96603af59a1c21131
OdongAlican <sandieo.2020@gmail.com>
2020-10-07 12:59:23 +0300

    update createActivity component

--- src/components/CreateActivity.jsx
+++ src/components/CreateActivity.jsx
@@ -4,17 +4,27 @@ import { useDispatch } from 'react-redux'
 
 const CreateActivity = ({ history }) => {
     const [title, setTitle] = useState('')
+    const [total, setTotal] = useState('')
     const dispatch = useDispatch()
     const submitAcvtivty = (event) => {
         event.preventDefault()
-        dispatch(createActivity({title}, history))
+        dispatch(createActivity({title, total}, history))
     }
 
     return (
         <div className="card col-md-3 p-3">
             <h3>Create Activity</h3>
             <form onSubmit={ submitAcvtivty }>
-                <input value = { title } onChange={ e => setTitle(e.target.value) } className="form-control"></input>
+                <input 
+                placeholder="Enter Title"
+                value = { title } 
+                onChange={ e => setTitle(e.target.value) } 
+                className="form-control mb-2"></input>
+                <input 
+                placeholder="Enter Total"
+                value = { total } 
+                onChange={ e => setTotal(e.target.value) } 
+                className="form-control"></input>
                 <button type="submit" className="btn btn-primary mt-1">Create</button>
             </form>
 
