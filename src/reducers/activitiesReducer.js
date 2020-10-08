47b59637ceaf6ab3a9a547f209b7296c1b8cfd23
OdongAlican <sandieo.2020@gmail.com>
2020-10-09 00:00:08 +0300

    update activity reducer file

--- src/reducers/activitiesReducer.js
+++ src/reducers/activitiesReducer.js
@@ -1,31 +1,43 @@
 import { FETCH_ACTIVITIES_SUCCESS, 
         FETCH_ACTIVITIES_FAILURE, 
-        DELETE_ACTIVITY} from '../actions/activitiesAction'
+        DELETE_ACTIVITY,
+        FETCH_ACTIVITIES_REQUEST
+        } from '../actions/activitiesAction'
 
 const initialState = {
     activities: [],
-    error: ''
+    error: '',
+    loading: false,
 }
 
 const activitiesReducer = ( state = initialState, action) => {
     switch(action.type){
+        case FETCH_ACTIVITIES_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            }
         case FETCH_ACTIVITIES_SUCCESS:
             return {
                 ...state,
                 activities: action.payload,
-                error: ''
+                error: '',
+                loading: false,
             }
         case FETCH_ACTIVITIES_FAILURE: 
             return {
                 ...state,
                 activities: [],
-                error: action.payload
+                error: action.payload,
+                loading: false,
+
             }
         case DELETE_ACTIVITY: 
             return {
                 ...state,
                 activities: state.activities.filter((data) => data.id !== action.payload),
-                error: ''
+                error: '',
+                loading: false
             }
         default:
             return state
