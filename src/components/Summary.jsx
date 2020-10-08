9a5591e04fee2a95ba56041ade8b5eb0a5d82574
OdongAlican <sandieo.2020@gmail.com>
2020-10-08 23:58:25 +0300

    update Summary component to display activity percentage

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 
-
 const Summary = ( { activities }) => {
+    let newArray = []
+        activities.forEach(element => {
+           newArray.push(parseInt(element.total)) 
+        });
+
+        let res = newArray.reduce((val, num) => val + num, 0)
+        console.log(res)
     return(
         <div className="d-flex">
             {
                 activities.map((val, index) => (
                     <div key = { index } className="card">
                         <p>{val.title}</p>
+                        <div>
+                            {
+                            parseFloat( 
+                                ((parseInt(val.total) / res)
+                                ) * 100).toFixed(2)
+                            } %
+                        </div>
                     </div>
                 ))
             }
