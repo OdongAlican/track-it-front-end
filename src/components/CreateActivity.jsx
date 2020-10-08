46e68f82c63596e8d29c30db1e087be33a434381
OdongAlican <sandieo.2020@gmail.com>
2020-10-08 17:16:32 +0300

    add image funtionality

--- src/components/CreateActivity.jsx
+++ src/components/CreateActivity.jsx
@@ -5,10 +5,18 @@ import { useDispatch } from 'react-redux'
 const CreateActivity = ({ history }) => {
     const [title, setTitle] = useState('')
     const [total, setTotal] = useState('')
+    const [image, setImage] = useState(null)
     const dispatch = useDispatch()
+
     const submitAcvtivty = (event) => {
+        
+        const form = new FormData()
+        form.append("title", title)
+        form.append("total", total)
+        form.append("avatar", image)
+        dispatch(createActivity(form, history))
         event.preventDefault()
-        dispatch(createActivity({title, total}, history))
+
     }
 
     return (
@@ -16,18 +24,26 @@ const CreateActivity = ({ history }) => {
             <h3>Create Activity</h3>
             <form onSubmit={ submitAcvtivty }>
                 <input 
+                type="text"
                 placeholder="Enter Title"
                 value = { title } 
                 onChange={ e => setTitle(e.target.value) } 
                 className="form-control mb-2"></input>
                 <input 
+                type="number"
                 placeholder="Enter Total"
                 value = { total } 
                 onChange={ e => setTotal(e.target.value) } 
                 className="form-control"></input>
-                <button type="submit" className="btn btn-primary mt-1">Create</button>
+                <input
+                type='file'
+                className="mt-2"
+                onChange={(e) => setImage(e.target.files[0])}
+                ></input>
+                <button type="submit" className="btn btn-primary mt-1">
+                    Create
+                </button>
             </form>
-
         </div>
     )
 }
