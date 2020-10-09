596e9dfd393562274d8e17a96aead5618fba4720
OdongAlican <sandieo.2020@gmail.com>
2020-10-09 16:18:16 +0300

    add Private Routes to the Route component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Signout from './auth/Signout'
 
 const Navbar = () => {
     return(
         <div className="col-md-12 d-flex pt-4 bg-info text-white justify-content-center">
             <h3>Track it</h3>
+            <div>
+                <Signout/>
+            </div>
         </div>
     )
 }
--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import App from './App'
 import Signin from './auth/Signin'
 import Activities from '../container/Activities'
 import Navbar from './Navbar'
 import CreateActivity from './CreateActivity'
 import Measurements from '../container/Measurements'
 import CreateMeasurement from '../components/CreateMeasurement'
+import PrivateRoute from './PrivateRoute'
 
 const Router = () => {
     return (
@@ -14,12 +14,11 @@ const Router = () => {
            <Navbar/> 
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" component={ App } exact/>
-                    <Route path="/signin" component= { Signin }/>
-                    <Route path="/activities" component= { Activities }/>
-                    <Route path="/create-activity" component={CreateActivity}/>
-                    <Route path="/activity/:id/measurements" component={ Measurements }/>
-                    <Route path="/activity/:id/create" component={ CreateMeasurement }/>
+                    <Route exact path="/signin" component= { Signin }/>
+                    <PrivateRoute exact path="/activities" component= { Activities }/>
+                    <PrivateRoute exact path="/create-activity" component={CreateActivity}/>
+                    <PrivateRoute exact path="/activity/:id/measurements" component={ Measurements }/>
+                    <PrivateRoute exact path="/activity/:id/create" component={ CreateMeasurement }/>
                 </Switch>
             </BrowserRouter>
         </div>
