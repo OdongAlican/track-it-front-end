d27aaa289d814b254768bab90ca8187f7966fb4e
OdongAlican <sandieo.2020@gmail.com>
2020-10-09 16:16:03 +0300

    add sign out component

---
+++ src/components/auth/Signout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {signOut} from '../../actions/index'
+import { useDispatch } from 'react-redux'
+
+const Signout = () => {
+
+const dispatch = useDispatch()
+
+const logOut = () => {
+    dispatch(signOut())
+}
+
+    return(
+        <div>
+            <button 
+            onClick={ logOut }
+            className="btn btn-secondary">Sign Out</button>
+        </div>
+    )
+}
+
+export default Signout
