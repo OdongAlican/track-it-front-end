9de1e4deb6f50b983087e0138f0dbc54528d0ace
OdongAlican <sandieo.2020@gmail.com>
2020-10-12 06:57:44 +0300

    update styles in Summary component

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -9,20 +9,55 @@ const Summary = ( { activities }) => {
         let res = newArray.reduce((val, num) => val + num, 0)
         console.log(res)
     return(
-        <div className="d-flex">
+        <div className="d-flex summary-section">
             {
-                activities.map((val, index) => (
-                    <div key = { index } className="card">
-                        <p>{val.title}</p>
-                        <div>
-                            {
-                            parseFloat( 
-                                ((parseInt(val.total) / res)
-                                ) * 100).toFixed(2)
-                            } %
+                activities.map((val, index) => {
+                    let percentageValue = parseInt(((parseInt(val.total) / res)) * 100)
+                    if(percentageValue <= 25) {
+                    return(
+                        <div key = { index } className=" card-stuff p-2">
+                            <div className="percentage-section" 
+                            style={{ "border-right" : "5px solid #94E28E"}}>
+                                { percentageValue } %
+                            </div>
+                            <div className="title-section"><p>{val.title}</p></div>
                         </div>
-                    </div>
-                ))
+                    )
+                    } else if ( percentageValue > 25 && percentageValue <= 50) {
+                        return(
+                        <div key = { index } className=" card-stuff p-2">
+                            <div className="percentage-section" 
+                            style={{ "border-right" : "5px solid #F24429",
+                                    "border-bottom" : "5px solid #F24429"}}>
+                                { percentageValue } %
+                            </div>
+                            <div className="title-section"><p>{val.title}</p></div>
+                        </div>
+                        )
+                    } else if ( percentageValue > 50 && percentageValue <= 75) {
+                        return(
+                        <div key = { index } className=" card-stuff p-2">
+                            <div className="percentage-section" 
+                            style={{ "border-right" : "5px solid #94E28E",
+                                    "border-bottom" : "5px solid #94E28E",
+                                    "border-left" : "5px solid #94E28E"}}>
+                                { percentageValue } %
+                            </div>
+                            <div className="title-section"><p>{val.title}</p></div>
+                        </div>
+                        )
+                    } else {
+                        return(
+                            <div key = { index } className=" card-stuff p-2">
+                            <div className="percentage-section" 
+                            style={{ "border" : "5px solid #F24429"}}>
+                                { percentageValue } %
+                            </div>
+                            <div className="title-section"><p>{val.title}</p></div>
+                        </div>
+                        )
+                    }
+                })
             }
         </div>
     )
