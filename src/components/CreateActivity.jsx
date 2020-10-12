28c3f37ac76563b8848b56b61ef7f819a24d5c95
OdongAlican <sandieo.2020@gmail.com>
2020-10-12 18:06:27 +0300

    update create activity component

--- src/components/CreateActivity.jsx
+++ src/components/CreateActivity.jsx
@@ -21,31 +21,43 @@ const CreateActivity = ({ history }) => {
     }
 
     return (
+        <div className="create-activity">
         <div>
-        <div className="card col-md-3 p-3">
-            <h3>Create Activity</h3>
-            <form onSubmit={ submitAcvtivty }>
+            <div className="activity-heading mb-2 p-3">
+                <span>You can add a photo to your activity</span>
+            </div>
+            <div className="image-upload m-3 p-2 d-flex">
+                <div className="left-image">
+                    <i class="fas fa-tv"></i>
+                </div>
+                <div className="right-image">
+                    <i class="fas fa-church"></i>
+                </div>
+            </div>
+            <div>
+                <form onSubmit={ submitAcvtivty }>
                 <input 
                 type="text"
                 placeholder="Enter Title"
                 value = { title } 
                 onChange={ e => setTitle(e.target.value) } 
-                className="form-control mb-2"></input>
+                className="input-control mb-2"></input>
                 <input 
                 type="number"
                 placeholder="Enter Total"
                 value = { total } 
                 onChange={ e => setTotal(e.target.value) } 
-                className="form-control"></input>
+                className="input-control"></input>
                 <input
                 type='file'
-                className="mt-2"
+                className="mt-2 btn-control file-section"
                 onChange={(e) => setImage(e.target.files[0])}
                 ></input>
-                <button type="submit" className="btn btn-primary mt-1">
+                <button type="submit" className="btn-control mt-1">
                     Create
                 </button>
             </form>
+            </div>
         </div>
         <Footer/>
         </div>
