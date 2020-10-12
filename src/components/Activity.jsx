67619242684ae0be2f36a072bb95ff500c1baf21
OdongAlican <sandieo.2020@gmail.com>
2020-10-12 06:45:46 +0300

    update styles in Activity component

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -12,23 +12,27 @@ const Activity = ({ activity }) => {
     }
 
     return (
-        <div className="card col-md-3 mt-2" >
-            <img src={ activity.avatar.url } alt="boohoo" className="image-det"/>
-            <div className="card-body">
-                <h5 className="card-title">{ activity.title }</h5>
-                <h5 className="card-title">{ activity.total }</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <button className="btn btn-secondary mr-2">Edit</button>
-                <button className="btn btn-info mr-2">
-                    <Link className="text-white" to={{
+        <div className="main-section d-flex p-1 m-2 bg-white" >
+            <div className="w-50 bg-secondary image-section">
+                <img src={ activity.avatar.url } alt="boohoo" className="image-det"/>
+            </div>
+            <div className="activity-body ml-1">
+                <div>
+                    <span className="activity-title text-secondary">{ activity.title }</span>
+                </div>
+                <div>
+                    <span className="activity-text text-secondary">{ activity.total }</span>
+                </div>
+                <div className="lower-section">
+                    <Link className="links-info" to={{
                         pathname: `/activity/${activity.id}/measurements`,
-                        state: activity.id 
+                        state: activity.id,
+                        actTitle: activity.title
                     }}>
-                        View Details
+                        Details
                     </Link>
-                    </button>
-                <button className="btn btn-danger" onClick = {() => deleteAct(activity.id) }>Delete</button>
+                    <i className="far fa-trash-alt ml-4 text-secondary" onClick = {() => deleteAct(activity.id) }></i>
+                </div>
             </div>
         </div>
     )
