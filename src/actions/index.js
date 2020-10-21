50a053c858f975cf7290f002c3467f111dc8d1c7
OdongAlican <sandieo.2020@gmail.com>
2020-10-21 20:07:07 +0300

    update authentication actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,36 +1,39 @@
-import axios from 'axios';
+import { sendUnauthenticatedRequest } from '../utils/api';
 
 export const AUTHENTICATED = 'authenticated_user';
 export const UNAUTHENTICATED = 'unauthenticated_user';
 export const AUTHENTICATION_ERROR = 'authentication_error';
 
-const URL = 'https://enigmatic-cliffs-07216.herokuapp.com';
-
 export function signInAction({ username, password }, history) {
   return async dispatch => {
-    const response = await axios.post(`${URL}/login`, { username, password });
-    if (response.data.token) {
+    const path = 'login';
+    const method = 'post';
+    const data = { username, password };
+    try {
+      const response = await sendUnauthenticatedRequest(method, path, data);
       dispatch({ type: AUTHENTICATED });
       localStorage.setItem('user', response.data.token);
       history.push('/activities');
-    } else {
+    } catch (error) {
       dispatch({
         type: AUTHENTICATION_ERROR,
         payload: 'Invalid username or Password',
       });
-      history.push('/');
     }
   };
 }
 
 export function signUpAction({ username, password, age }, history) {
   return async dispatch => {
-    const response = await axios.post(`${URL}/users`, { username, password, age });
-    if (response.data.token) {
+    const path = 'users';
+    const method = 'post';
+    const data = { username, password, age };
+    try {
+      const response = await sendUnauthenticatedRequest(method, path, data);
       dispatch({ type: AUTHENTICATED });
       localStorage.setItem('user', response.data.token);
       history.push('/activities');
-    } else {
+    } catch (error) {
       dispatch({
         type: AUTHENTICATION_ERROR,
         payload: 'Invalid User credentials',
